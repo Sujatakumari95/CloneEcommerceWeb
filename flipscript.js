78403868a31b0e54cf648c782e82f7481ab0cfe3
Sujata Yadav <sujatay51@gmail.com>
2025-05-19 14:21:30 +0530

    Update flipscript.js

--- flipscript.js
+++ flipscript.js
@@ -5,7 +5,7 @@ const menu = document.getElementById("mySideMenu");
     const slide = document.getElementById("bar");
    
      slide.addEventListener("click" , () =>{
-        console.log("hii");
+       
         menu.style.width = "50%";
      });
      function toggleMenu() {
